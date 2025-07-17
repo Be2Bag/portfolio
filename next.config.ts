03b295cc3ba791fc1116114dc72a3190d99b4299
Panupong Songsaksri <songskisri@gmail.com>
2025-07-17 10:19:38 +0700

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // ✅ เพิ่มบรรทัดนี้
+  // คุณสามารถเพิ่ม config อื่นได้ตรงนี้ เช่น images, basePath ฯลฯ
 };
 
 export default nextConfig;
